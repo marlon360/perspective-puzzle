7a3c4dee32dcf5f8c60b8eff7595da034c7fd2a8
Marlon Lückert <m.lueckert@me.com>
2019-02-04 22:20:09 +0100

    rotate and translate world

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public float rotationSensitivity = 0.4f;
+    public float translationSensitivity = 0.001f;
+
+    private Vector3 reference;
+    private Vector3 offset;
+
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 translation = Vector3.zero;
+
+    private bool isDragging;
+
+    void Update () {
+
+        if (Input.GetMouseButtonDown (0)) {
+            // rotating flag
+            isDragging = true;
+
+            // store mouse
+            reference = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp (0)) {
+            // rotating flag
+            isDragging = false;
+        }
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch (0);
+            if (touch.phase == TouchPhase.Began) {
+                // rotating flag
+                isDragging = true;
+
+                // store touch
+                reference = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Ended) {
+                // rotating flag
+                isDragging = false;
+            }
+
+            if (Input.GetTouch (0).phase == TouchPhase.Stationary || Input.GetTouch (0).phase == TouchPhase.Moved) {
+                offset = (touch.position - new Vector2 (reference.x, reference.y));
+                // store touch
+                reference = touch.position;
+            }
+        } else {
+            // offset
+            offset = (Input.mousePosition - reference);
+            // store mouse
+            reference = Input.mousePosition;
+        }
+
+        if (isDragging) {
+
+            // apply rotation
+            rotation.y = -offset.x * rotationSensitivity;
+
+            // apply translation
+            translation.y = offset.y * translationSensitivity;
+
+            // rotate
+            transform.Rotate (rotation);
+            // translate
+            transform.Translate (translation);
+
+        }
+    }
+}
