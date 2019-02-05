ef26546519104bc4220ce032e3af95b682974d37
Marlon Lückert <m.lueckert@me.com>
2019-02-05 16:59:35 +0100

    added animation to player

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -6,9 +6,17 @@ using UnityEngine.AI;
 public class MoveTo : MonoBehaviour {
 
     private NavMeshAgent agent;
+    private Animator anim;
 
     void Start () {
         this.agent = GetComponent<NavMeshAgent> ();
+        this.anim = GetComponent<Animator>();
+        this.anim.SetBool("Grounded", true);
+    }
+
+    void Update() {
+        this.anim.SetFloat("MoveSpeed", this.agent.velocity.magnitude / this.agent.speed / 1.6f);
+        
     }
 
     public void SetDestination (Vector3 destination) {
